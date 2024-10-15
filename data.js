e7993a5982e174dc850d7951762995915740b38d
Siddhant Sathe <142108985+SiddhantSathe@users.noreply.github.com>
2024-10-15 22:18:25 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,28 @@
+const products = [
+    {
+      id: 1,
+      name: "HP Laptop 15.6",
+      image:
+      "https://th.bing.com/th/id/OIP.OU1CoSplSgGv9VpKxYneSAHaFc?w=254&h=187&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+      price: 399.95,
+      desc: `Hp Laptop 15.6" 15-dy1036nr 10th Gen Intel Core i5-1035G1 8GB DDR4 256GB SSD`,
+    },
+    {
+      id: 2,
+      name: "Asus VivoBook 15",
+      image:
+        "https://th.bing.com/th/id/OIP.i93kgvtClw5v9M1XLz9NQgHaHa?w=187&h=187&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+      price: 299.98,
+      desc: `Asus VivoBook 15 F515 Thin and Light Laptop, 15.6” FHD Display, Intel Core i3-1005G1 Processor, 4GB DDR4 RAM, 128GB PCIe SSD, Fingerprint Reader, Windows 10 Home in S Mode, Slate Grey, F515JA-AH31`,
+    },
+    {
+      id: 3,
+      name: "acer travelmate",
+      image:
+        "https://th.bing.com/th/id/OIP.5EZRHGR0LgL2IWcQ511TkQHaF5?w=235&h=187&c=7&r=0&o=5&dpr=1.1&pid=1.7",
+      price: 310.99,
+      desc: `Acer TravelMate P6 Thin & Light Business Laptop, 14" Full HD, Intel Core i7-8565U, 16GB DDR4, 512GB PCIe NVMe SSD, 802.11ac Wi-Fi, 10 Hrs Battery, Win 10 Pro, TMP 414-51-72KR`,
+    },
+  ];
+
+  module.exports = { products };
